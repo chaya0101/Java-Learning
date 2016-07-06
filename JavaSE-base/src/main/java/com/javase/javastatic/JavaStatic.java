72a38d516d90165291bc263784d0c12105edff13
liaosl <975385373@qq.com>
2016-07-06 22:51:21 +0800

    Java中的static关键字，包括静态常量，变量，静态方法，静态块，静态类

---
+++ JavaSE-base/src/main/java/com/javase/javastatic/JavaStatic.java
@@ -0,0 +1,33 @@
+package com.javase.javastatic;
+
+/**
+ * Created by 廖双龙 on 2016/7/6.
+ * Java 当中Static的用法，这也是面试了很多家公司都爱考的东西
+ *
+ */
+public class JavaStatic {
+    public static  void main(String[] args){
+        ParentClass parentClass = new ChildClass();
+    }
+}
+
+class ParentClass{
+
+    public ParentClass(){
+        System.out.println("父类构造器");
+    }
+    static {
+        System.out.println("父类静态块");
+    }
+}
+
+class ChildClass extends  ParentClass{
+
+    public ChildClass(){
+        System.out.println("子类构造器");
+    }
+
+    static{
+        System.out.println("子类静态块");
+    }
+}
