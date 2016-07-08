d7c8def755a58ced1aaba008734247a4df780a65
liaosl <975385373@qq.com>
2016-07-08 22:28:30 +0800

    Java当中for循环的执行顺序

---
+++ JavaSE-base/src/main/java/com/javase/javaloop/ForLoop.java
@@ -0,0 +1,22 @@
+package com.javase.javaloop;
+
+/**
+ * Created by 廖双龙 on 2016/7/8.
+ * Java for 循环的执行顺序
+ */
+public class ForLoop {
+    public  static int i=10;
+    public static void main(String[] args){
+        for(i=0;++i<10;i++){
+            //一般for循环的写法为：
+            // for(c1;c2;c3){
+            //      一般的执行顺序是，c1,c2,statement,c3，然后再执行c2,statement,c3循环下去
+            //  statement(执行语句)
+            // }
+
+        }
+    }
+
+}
+
+
