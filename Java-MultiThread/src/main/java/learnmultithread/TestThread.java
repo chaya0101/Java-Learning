d42d9ad6ad400b96bdf20e8211f2c1c5ef908bae
liaosl <975385373@qq.com>
2016-07-15 12:37:23 +0800

    心情总是很低落，上传了一点无关痛痒的东西，准备着手学习多线程了

---
+++ Java-MultiThread/src/main/java/learnmultithread/TestThread.java
@@ -0,0 +1,11 @@
+package learnmultithread;
+
+/**
+ * Created by 廖双龙 on 2016/7/15.
+ */
+public class TestThread  implements  Runnable{
+
+    public void run() {
+
+    }
+}
---
+++ Java-MultiThread/src/main/java/learnmultithread/TetsIn.java
@@ -0,0 +1,9 @@
+package learnmultithread;
+
+/**
+ * Created by 廖双龙 on 2016/7/15.
+ */
+public interface TetsIn {
+    default void test(){
+    }
+}
