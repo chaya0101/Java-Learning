0940a599c66893ea83dd1565382ce5d0b634ed53
liaosl <975385373@qq.com>
2017-04-19 21:20:34 +0800

    简单中文计算器相关方法

--- Java-programing/src/main/java/com/javaprogram/algorithm/SimpleCalculate.java
+++ Java-programing/src/main/java/com/javaprogram/algorithm/SimpleCalculate.java
@@ -1,13 +1,46 @@
 package com.javaprogram.algorithm;
 
-/**
- * Created by 廖双龙 on 2016/7/20.
- */
+import java.util.Scanner;
 
 /**
  * 实现简单的中文计算器
- * 如：
+ * 如：输入二十加五 输出：二十五
+ * 简单的加减法
+ * @author 廖双龙
  */
 public class SimpleCalculate {
+
+    public static  void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        System.out.print(input);
+    }
+
+    /**
+     * 中文分词，将中文转化成数字再计算
+     * @param input
+     * @return
+     */
+    public String calcuate(String input) {
+        String output = "";
+        input = input.replaceAll("加","+");
+        input = input.replaceAll("减","-");
+        input = input.replaceAll("乘","*");
+        input = input.replaceAll("除","/");
+        return  output;
+    }
+
+    public String transStringToNum(String input){
+        String output = "";
+
+        return  output;
+    }
+
+    public String transNumToString(String input){
+        String output = "";
+
+        return  output;
+    }
 
 }
